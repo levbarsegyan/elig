c9b54ec6b7215c3e7a89fb5fbf3ae33b8c9d2b00
Lev <lev.barsegyan@devit-team.com>
2021-12-04 14:30:23 +0000

    elioMaster Mon 30 Jul 20:27:44 EEST 2018

--- tribe.sh
+++ tribe.sh
@@ -5,7 +5,7 @@ timestamp="$(date)";
 for f in elioBelievers elioData elioSin elioThings
 do
   cd $f;
-  source "./$f/tribe.sh" "$TRIBE" "$COMMAND";
+  source "_$COMMAND.sh" "$TRIBE" "$COMMAND";
   cd ..;
 done
 echo "[>><<TRIBES>><<].$TRIBE.elioBelievers._$COMMAND ✓";
