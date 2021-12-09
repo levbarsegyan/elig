d5524d220ec9108551923fcc07363353bfbf1768
Lev <lev.barsegyan@devit-team.com>
2021-12-09 17:22:12 +0000

    elioMaster Tue 31 Jul 01:10:50 EEST 2018

--- elioBelievers/tribe.sh
+++ elioBelievers/tribe.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-set -e
+set +e
 TRIBE="$1";
 COMMAND="$2";
 EXTRA="$3";
-timestamp="$(date)";
-for APP in sinsay tribes
+while IFS='' read -r line || [[ -n "$line" ]];
 do
-  cd $APP;
+  cd $line;
   source "../../_$COMMAND.sh" "$TRIBE" "$EXTRA";
-  source "../../_say.sh" "$TRIBE" "$COMMAND" "$APP";
+  source "../../_say.sh" "$TRIBE" "$COMMAND" "$line";
   cd ..;
-done
+done < ".tribalinclude"
 PROJECT=${PWD##*/};
-source "../_$COMMAND.sh" "$TRIBE" "$EXTRA ";
+source "../_$COMMAND.sh" "$TRIBE" "$EXTRA";
 source "../_say.sh" "$TRIBE" "$COMMAND" "$PROJECT";
--- tribe.sh
+++ tribe.sh
@@ -3,12 +3,11 @@ set +e
 TRIBE="$1";
 COMMAND="$2";
 EXTRA="$3";
-for PROJ in elioData #elioBelievers elioThings # elioData elioSin elioThings
-do
-  cd $PROJ;
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  cd $line;
   source "tribe.sh" "$TRIBE" "$COMMAND" "$EXTRA";
   cd ..;
-done
-KING=${PWD##*/};
+done < ".tribalinclude"
+PROJECT=${PWD##*/};
 source "_$COMMAND.sh" "$TRIBE" "$EXTRA";
-source "_say.sh" "$TRIBE" "$COMMAND" "$KING";
+source "_say.sh" "$TRIBE" "$COMMAND" "$PROJECT";
