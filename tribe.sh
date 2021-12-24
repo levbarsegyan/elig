ac741711925d0ebb16702fb734f67d0e70012018
Lev <lev.barsegyan@devit-team.com>
2021-12-24 14:45:12 +0000

    master Tue 31 Jul 02:37:53 EEST 2018

--- tribe.sh
+++ tribe.sh
@@ -3,11 +3,11 @@ set +e
 TRIBE="$1";
 COMMAND="$2";
 EXTRA="$3";
+PROJECT=${PWD##*/};
+source "_say.sh" "$TRIBE" "$COMMAND" "$PROJECT";
+source "_$COMMAND.sh" "$TRIBE" "$EXTRA";
 while IFS='' read -r line || [[ -n "$line" ]]; do
   cd $line;
   source "tribe.sh" "$TRIBE" "$COMMAND" "$EXTRA";
   cd ..;
 done < ".tribalinclude"
-PROJECT=${PWD##*/};
-source "_say.sh" "$TRIBE" "$COMMAND" "$PROJECT";
-source "_$COMMAND.sh" "$TRIBE" "$EXTRA";
